bd9553575c013f4c7fc4499d86e957f51971aaf4
Siddharth Jain <sid26@ucla.edu>
2017-03-09 17:38:41 +0530

    refactor

---
+++ build.js
@@ -0,0 +1,2 @@
+Object.defineProperty(exports,"__esModule",{value:true});var _Toast=require('./lib/Toast');
+Object.keys(_Toast).forEach(function(key){if(key==="default"||key==="__esModule")return;Object.defineProperty(exports,key,{enumerable:true,get:function get(){return _Toast[key];}});});var _Toast2=_interopRequireDefault(_Toast);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.default=_Toast2.default;
